28bae9161942bd8a79c7ec1d2ba460179277d7f8
AndLego <galeon.gb@gmail.com>
2022-12-05 20:24:34 -0500

    fixed typo at prelude, and bugs with mozilla explorer

--- src/components/Prelude/Prelude.jsx
+++ src/components/Prelude/Prelude.jsx
@@ -8,7 +8,7 @@ const Prelude = ({ language }) => {
     English: {
       hi: "Hi!",
       who: "I'm Andres Leon",
-      job: "I'am a FullStack Developer - FrontEnd Specialized. Based in Bogotá, Colombia",
+      job: "I'm a FullStack Developer - FrontEnd Specialized. Based in Bogotá, Colombia",
     },
     Español: {
       hi: "Hola!",
@@ -22,42 +22,6 @@ const Prelude = ({ language }) => {
     : (content = content.English);
 
   return (
-    // <article data-aos="zoom-in" className={styles.wrapper}>
-    //   <div className={styles.container}>
-    //     <div ref={scene} className={styles.scene} data-hover-only="false">
-    //       <div className={styles.circle} data-depth="1.2" />
-    //       <div className={styles.one} data-depth="0.9">
-    //         <div className={styles.content}>
-    //           <span className={styles.piece} />
-    //           <span className={styles.piece} />
-    //           <span className={styles.piece} />
-    //         </div>
-    //       </div>
-    //       <div className={styles.two} data-depth="0.60">
-    //         <div className={styles.content}>
-    //           <span className={styles.piece} />
-    //           <span className={styles.piece} />
-    //           <span className={styles.piece} />
-    //         </div>
-    //       </div>
-    //       <div className={styles.three} data-depth="0.40">
-    //         <div className={styles.content}>
-    //           <span className={styles.piece} />
-    //           <span className={styles.piece} />
-    //           <span className={styles.piece} />
-    //         </div>
-    //       </div>
-    //       {/* <p className={styles.p404} data-depth="0.50">
-    //         {content.hi}
-    //       </p>
-    //       <p className={styles.p404} data-depth="0.10">
-    //         {content.hi}
-    //       </p> */}
-    //     </div>
-    //     <div className={styles.text}>
-    //     </div>
-    //   </div>
-    // </article>
     <section className={styles.Prelude}>
       <p className={styles.p404} data-depth="0.50">
         {content.hi}
